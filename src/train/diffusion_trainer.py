9610e99eeb0ccb1c740dab7ccffa042670d721ca
GAD-cell <gadsinoue@gmail.com>
2025-10-13 17:54:18 +0200

    base trainer

---
+++ src/train/diffusion_trainer.py
@@ -0,0 +1,25 @@
+from transformers import Trainer
+
+class A2DTrainer(Trainer):
+    def __init__(self, model, tokenizer, args):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.args = args
+    
+    def _prepare_inputs(self, batch)
+        pass
+    
+    def compute_loss(self, model,inputs):
+
+        
+        loss = #compute loss
+        return loss
+
+    def training_step(self, model, inputs, num_items_in_batch=None) -> torch.Tensor:
+        
+        inputs = self._prepare_inputs(inputs)
+        model.train()
+        loss = self.compute_loss(model,inputs)
+
+
+        return loss.detach()
